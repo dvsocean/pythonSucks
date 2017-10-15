c957b771acfe0a7204e9514d7e0c49690d594588
dvsocean <daniel.ocean@aa.com>
2017-10-15 12:07:04 -0700

    classes

---
+++ person.py
@@ -0,0 +1,12 @@
+# Daniel Ocean 
+# CISW 24 LAB
+# 10/15/17
+# Program 4 
+
+class Person:
+
+	a = "THIS IS TEST"
+
+	def __init__(self, name, surname):
+		self.name = name
+		self.surname = surname
---
+++ student.py
@@ -0,0 +1,16 @@
+# Daniel Ocean 
+# CISW 24 LAB
+# 10/15/17
+# Program 4 
+from person import a
+
+class Student:
+
+
+
+
+
+
+obj = Person("JOE")
+
+print(obj.a)
