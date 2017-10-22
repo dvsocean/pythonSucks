a180820012eef3c5a12ff40745a89e9060907a3b
dvsocean <dvsocean@icloud.com>
2017-10-21 18:58:50 -0700

    finished classes assignment

--- person.py
+++ person.py
@@ -24,13 +24,36 @@ class Person:
 	def getLastName(self):
 		return self.last_name
 
-	def __repr__(self):
-		return '[Person: %s %s]' % (self.first_name, self.last_name)
+	# def __repr__(self):
+	# 	return '[Person: %s %s]' % (self.first_name, self.last_name)
+
+	def __str__(self):
+		return "[Person: " + self.first_name + " " + self.last_name + "]"
+
+
+#INSTRUCTIONS WERE NOT CLEAR
+
+# constructed Person with defaults NoFirstName NoLastName
+# first_name NoFirstName
+# last_name NoLastName
+# set first and last names
+# James Holbert
+# constructed Person with explicit arguments Johnny Bravo
+
 
 if __name__ == '__main__':
-	print("constructed Person with defaults")
-	print("NoFirstName NoLastName")
-	
+	obj = Person()						# constructed Person with defaults NoFirstName NoLastName
+	print(obj)
+	print(obj.getFirstName())			# first_name NoFirstName
+	print(obj.getLastName())			# last_name NoLastName
+	obj2 = Person("Johnny", "Bravo")	# constructed Person with explicit arguments Johnny Bravo
+	print(obj2)
+
+print("----------------------------------PERSON CLASS ABOVE THIS LINE--------------------------------")
+
+
+
+
 	
 
 
